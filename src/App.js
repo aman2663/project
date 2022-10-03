f098a5a6458eb49a6f3741a298711045c0d87a9e
aman2663 <amakesharin2663@gmail.com>
2022-10-03 19:52:46 +0530

    changed css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Collection from './Components/Collection/Collection';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
